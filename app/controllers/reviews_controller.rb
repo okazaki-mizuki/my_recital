ca8cc5c09369f12f41d31542836d2207e9eeb1e1
Okazaki Mizuki <hpylh2ek@gmail.com>
2025-10-24 20:47:20 +0900

    エラー修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
