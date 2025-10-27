404920b3e02565cfc1885c58cc2e99d6c6a1e081
Okazaki Mizuki <hpylh2ek@gmail.com>
2025-10-27 10:08:55 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,3 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
-
-  protected
-
-  def authenticate_user!(opts = {})  # ← メソッド定義が必要
-    return super if user_signed_in?
-
-    respond_to do |format|  # ← メソッドの中に入れる
-      format.html { redirect_to new_user_session_path, alert: "ログインしてください。" and return }
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace(
-          "flash",
-          partial: "shared/flash_message",
-          locals: { alert: "ログインしてください。" }
-        )
-        return
-      end
-    end
-  end  # ← メソッドの終了
 end
